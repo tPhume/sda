37060ae3d12f0b068b50defbcd00c358ff22f4f0
Traphume <traphumedev@gmail.com>
2020-01-13 10:19:36 +0700

    Initial init

---
+++ src/lab1/pattern/Observable.java
@@ -0,0 +1,58 @@
+package lab1.pattern;
+ 
+import java.util.ArrayList;
+
+
+/**
+ * An abstract class for all Observable subjects
+ */
+public abstract class Observable {
+	/**
+	 * Constructs an Observable object
+	 */
+	public Observable() {
+		this.observers = new ArrayList<Observer>();
+	}
+
+	/**
+	 * Attach to this Subject
+	 * 
+	 * @param o
+	 *            the Observer that wishes to attach
+	 */
+	public void attach(Observer o) {
+		this.observers.add(o);
+	}
+
+	/**
+	 * Detach from this Subject
+	 * 
+	 * @param o
+	 *            the Observer that wishes to detach
+	 */
+	public void detach(Observer o) {
+		for (int i = 0; i < observers.size(); i++) {
+			if (observers.get(i).equals(o))
+				observers.remove(i);
+		}
+	}
+
+	/**
+	 * Notify all Observers that Subject has changed
+	 */
+	public void notifyObservers() {
+		for (int i = 0; i < observers.size(); i++) {
+			Observer observer = observers.get(i);
+			observer.update(this);
+		}
+	}
+
+	/**
+	 * Pull updated data from this Subject
+	 * 
+	 * @return the updated data from the Subject
+	 */
+	public abstract Object getUpdate();
+
+	protected ArrayList<Observer> observers;
+}
